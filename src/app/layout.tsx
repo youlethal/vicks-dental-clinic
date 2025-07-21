a62fdf3176011bd2d55a22cd404cd6683b2790fc
youlethal <78674543+youlethal@users.noreply.github.com>
2025-07-21 22:43:49 +0100

    Add src/app/layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+"use client"
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Vicks Dental Clinic  - Professional Dentist Services',
+  description: 'Professional Dentist services with Appointment Booking, Service List, Team Profiles, Patient Reviews. Contact us today for exceptional service.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
